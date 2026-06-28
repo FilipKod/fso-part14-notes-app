83b956096fa73d806e2aa07fe5f2f605d45ef6b7
Filip Madunicky <yelidric@gmail.com>
2026-06-28 13:50:35 +0200

    Add note detail page and update notes listing with links

--- app/services/notes.ts
+++ app/services/notes.ts
@@ -17,3 +17,7 @@ export const getNotes = () => {
 export const addNote = (content: string, important: boolean) => {
   notes.push({id: nextId++, content, important})
 }
+
+export const getNoteById = (id: number) => {
+  return notes.find((note) => note.id === id)
+}
